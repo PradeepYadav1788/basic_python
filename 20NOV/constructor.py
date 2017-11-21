65d6a0612866feb5c19c019e66196a95f2de729e
pradeep <pradeepyadav1788@gmail.com>
2017-11-21 16:51:17 +0530

    constructor parctice

---
+++ 20NOV/constructor.py
@@ -0,0 +1,11 @@
+class sample:
+    def __init__(self):
+        self.a=1000
+        self.b=2000
+    def display(self):
+        print(self.a)
+        print(self.b)
+        
+
+x1=sample()
+x1.display()
